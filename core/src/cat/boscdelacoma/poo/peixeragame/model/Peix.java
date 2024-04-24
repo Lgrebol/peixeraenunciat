713201c2666fe7c9b442164539aaa4e1ce21aeac
Lluís <Llus@172.24.2.102>
2024-04-24 10:50:40 +0200

    No "errors" Peixera bé

---
+++ core/src/cat/boscdelacoma/poo/peixeragame/model/Peix.java
@@ -0,0 +1,5 @@
+package cat.boscdelacoma.poo.peixeragame.model;
+
+public class Peix {
+    
+}
--- core/src/cat/boscdelacoma/poo/peixeragame/utils/FileUtils.java
+++ core/src/cat/boscdelacoma/poo/peixeragame/utils/FileUtils.java
@@ -1,29 +1,29 @@
-package cat.boscdelacoma.poo.peixeragame.utils;
+    package cat.boscdelacoma.poo.peixeragame.utils;
 
-import java.io.File;
-
-/**
- * Classe d'utilitats pel sistema de fitxers.
- *
- * @author Marc
- */
-public class FileUtils {
+    import java.io.File;
 
     /**
-     * Obté el nom d'un fitxer sense l'extensió
+     * Classe d'utilitats pel sistema de fitxers.
      *
-     * @param fullFilename el nom complet del fitxer (futa absoluta)
-     * @return el nom del fitxer sense path ni externsió.
+     * @author Marc
      */
-    public static String getFileNameWithoutExtension(String fullFilename) {
-        final String fileName = new File(fullFilename).getName();
-        final int dotIndex = fileName.lastIndexOf('.');
-        final String fileNameWithoutExtension;
-        if (dotIndex == -1) {
-            fileNameWithoutExtension = fileName;
-        } else {
-            fileNameWithoutExtension = fileName.substring(0, dotIndex);
+    public class FileUtils {
+
+        /**
+         * Obté el nom d'un fitxer sense l'extensió
+         *
+         * @param fullFilename el nom complet del fitxer (futa absoluta)
+         * @return el nom del fitxer sense path ni externsió.
+         */
+        public static String getFileNameWithoutExtension(String fullFilename) {
+            final String fileName = new File(fullFilename).getName();
+            final int dotIndex = fileName.lastIndexOf('.');
+            final String fileNameWithoutExtension;
+            if (dotIndex == -1) {
+                fileNameWithoutExtension = fileName;
+            } else {
+                fileNameWithoutExtension = fileName.substring(0, dotIndex);
+            }
+            return fileNameWithoutExtension;
         }
-        return fileNameWithoutExtension;
     }
-}
